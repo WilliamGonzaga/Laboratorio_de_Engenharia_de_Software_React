408fde65be1493e7b75471283d8d8508ba7c673a
WilliamGonzaga <williamgonzagadasantacruz@gmail.com>
2025-05-05 20:46:12 -0300

    Atualização 04 Contador de Pessoas

--- src/ContadorDePessoas.js
+++ src/ContadorDePessoas.js
@@ -4,7 +4,6 @@ import './ContadorDePessoas.css';
 export default function ContadorDePessoas() {
   const [homens, setHomens] = useState(0);
   const [mulheres, setMulheres] = useState(0);
-
   const total = homens + mulheres;
 
   const resetar = () => {
@@ -14,31 +13,31 @@ export default function ContadorDePessoas() {
 
   return (
     <div className="container">
-    <h1>Total</h1>
-    <div className="total-box">{total}</div>
-    <button className="reset" onClick={resetar}>🔄</button>
+      <button className="reset" onClick={resetar}>🔄</button>
+      <h1>Total</h1>
+      <div className="total-box">{total}</div>
 
-    <div className="grupo-container">
-      <div className="grupo">
-        <img src="/img/contadorDePessoas/foto-homem.png" alt="Homem" />
-        <div className="botoes">
-          <button onClick={() => setHomens(homens + 1)}>+</button>
-          <button onClick={() => setHomens(homens > 0 ? homens - 1 : 0)}>-</button>
+      <div className="grupo-container">
+        <div className="grupo">
+          <img src="/img/contadorDePessoas/foto-homem.png" alt="Homem" />
+          <div className="botoes">
+            <button className="mais" onClick={() => setHomens(homens + 1)}>+</button>
+            <button className="menos" onClick={() => setHomens(homens > 0 ? homens - 1 : 0)}>-</button>
+          </div>
+          <label>Homens</label>
+          <input value={homens} readOnly />
         </div>
-        <label>Homens</label>
-        <input value={homens} readOnly />
-      </div>
 
-      <div className="grupo">
-        <img src="/img/contadorDePessoas/foto-mulher.png" alt="Mulher" />
-        <div className="botoes">
-          <button onClick={() => setMulheres(mulheres + 1)}>+</button>
-          <button onClick={() => setMulheres(mulheres > 0 ? mulheres - 1 : 0)}>-</button>
+        <div className="grupo">
+          <img src="/img/contadorDePessoas/foto-mulher.png" alt="Mulher" />
+          <div className="botoes">
+            <button className="mais" onClick={() => setMulheres(mulheres + 1)}>+</button>
+            <button className="menos" onClick={() => setMulheres(mulheres > 0 ? mulheres - 1 : 0)}>-</button>
+          </div>
+          <label>Mulheres</label>
+          <input value={mulheres} readOnly />
         </div>
-        <label>Mulheres</label>
-        <input value={mulheres} readOnly />
       </div>
     </div>
-  </div>
   );
 }
