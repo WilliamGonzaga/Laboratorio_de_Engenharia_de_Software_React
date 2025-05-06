e8a950d461b5859f44325fa74fcf82e4b8438f91
WilliamGonzagadasantacru@z <williamgonzagadasantacruz@gmail.com>
2025-05-06 10:23:00 -0300

    atv3

---
+++ src/Atv03.js
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom"
+
+
+export default function Atv03() {
+    return (
+        <>
+
+            <h2>Atividade 3 </h2>
+
+
+            <Link to="/">Voltar</Link>
+
+        </>
+    )
+}
